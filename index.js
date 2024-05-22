bab56dbd803c1c0d740ec1ac42d7e44bcaeb9b9b
Alejandro Canosa <acanosa@users.noreply.github.com>
2024-05-22 18:12:22 -0300

    Feater/initial version (#3)
    * Rewrote history and added gitignore
    
    * Cleanup

---
+++ index.js
@@ -0,0 +1,39 @@
+const { ApolloError } = require('apollo-server-express');
+const diceLib = require("./dice")
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+
+
+const options = {
+    origin: "http://localhost:3000"
+}
+app.use(cors(options))
+
+app.use(express.json())
+
+app.listen(3001,  () => {
+    console.log("Server running on port 3001")
+})
+
+app.get("/", function(req, res) {
+    res.send("Roll a dice! execute POST /roll with your desired preferences to roll any dice you want up to D1000")
+});
+
+app.post("/roll", function(req, res) {
+    var facesList = req.body
+    if(facesList.length == 0) {
+        res.send(400, "You didnt roll any dice! for example, if you want to roll a d20, you must send [{'faces': 20}]")
+        return;
+    } else {
+        var responseBody = { "results": []}
+        for (var i = 0; i < facesList.length; i++) {
+            
+            var diceResult = diceLib.rollTheDice(facesList[i], i + 1);
+            responseBody.results.push(diceResult);
+        }
+        res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.send(responseBody );
+    }
+})
